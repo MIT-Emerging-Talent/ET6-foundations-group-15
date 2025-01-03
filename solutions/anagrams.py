6b6722ccdffe7a927c50eb218881edf28eb27c7e
theabdallahnjr <abdallahnjr0@gmail.com>
2025-01-03 20:48:05 +0200

    did some stuff

---
+++ solutions/anagrams.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+A module for checking whether two strings are anagrams.
+
+Module contents:
+    - are_anagrams: takes 2 strings as arguments and check whether they are anagrams.
+
+Created on 03 01 25
+@author: Abdallah Alnajjar
+"""
+
+
+def are_anagrams(word_1: str, word_2: str):
+    """
+    Checks whether two strings are anagrams.
+
+    Parameters:
+        word_1 (str): The first word (string) to be compared.
+        word_2 (str): The second word (string) to be compared.
+
+    Returns:
+        bool: "True" if the two string are anagrams,
+        and "False" if it's not the case
+
+    Raises:
+    AssertionError: If the input is not a string.
+
+
+    """
+
+    # Remove spaces and convert to lowercase for accurate comparison
+    word_1 = word_1.replace(" ", "").lower()
+    word_2 = word_2.replace(" ", "").lower()
+
+    # Check if sorted characters of both strings are equal
+    return sorted(word_1) == sorted(word_2)
